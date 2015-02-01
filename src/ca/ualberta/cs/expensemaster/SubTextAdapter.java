e9d1112a81a37e7893bf6774246b5a005055a0fe
Tristan Meleshko <tmeleshk@ualberta.ca>
2015-02-01 13:50:27 -0700

    Added License Headers

--- src/ca/ualberta/cs/expensemaster/SubTextAdapter.java
+++ src/ca/ualberta/cs/expensemaster/SubTextAdapter.java
@@ -1,3 +1,21 @@
+/* This file is part of the ExpenseMaster Android Application
+ * See github.com/ItsTristan/ExpenseMaster for more information
+ * 
+ * Copyright (C) 2015 Tristan Meleshko
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
 package ca.ualberta.cs.expensemaster;
 
 import java.util.ArrayList;
@@ -13,7 +31,7 @@ import android.widget.TextView;
 // http://www.softwarepassion.com/android-series-custom-listview-items-and-adapters/
 //   Jan 31, 2015
 // It might be better to extend a BaseAdapter here, but for straight-forward
-// substitution reasons, I'm extending ArrayAdapter here
+// substitution reasons, I'm extending ArrayAdapter here.
 // 
 /**
  * A prettier looking array adapter that allows subtext in listviews.
